3d98f54d0d2969e442b87ba3e1d76bd1f7b41319
Sean Pettit <s.p@mac.com>
2018-01-23 12:33:43 +0000

    added Typographyjs and Hedron

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,6 +11,12 @@ module.exports = {
   },
     'gatsby-plugin-react-helmet',
     `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography.js`,
+      },
+    },
   ],
 };
--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,41 +1,30 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import Img from 'gatsby-image';
+import {Page, Row, Column} from 'hedron';
 
 class BlogPost extends Component {
-
-renderImages(artworks) {
-  return artworks.map((ele) => {
-    // const imagePath = "https:"+ele.resize.src
-    // console.log(imagePath);
-    return(
-      <div className="fl w-third" key={ele.resolutions.src}>
-        <article className="mw5 mw6-ns center pt4">
-        <div className="aspect-ratio aspect-ratio--3x4 mb4">
+  renderImages(artworks) {
+    return artworks.map((ele) => {
+      return (
+        <Column fluid="fluid" sm={4} key={ele.resolutions.src}>
           <Img resolutions={ele.resolutions} className="aspect-ratio--object cover"/>
-        </div>
-        </article>
-      </div>
-
-    )
-  })
-}
-
-
+        </Column>)
+    })
+  }
 
   render() {
-    const { galleryName } = this.props.data.contentfulGallery
-    const { artworks }= this.props.data.contentfulGallery
-    // this.renderImages(artworks)
+    const {galleryName} = this.props.data.contentfulGallery
+    const {artworks} = this.props.data.contentfulGallery
     return (
-    <div>
-      <div className="f1">{galleryName}</div>
       <div>
-        {this.renderImages(artworks)}
-      </div>
-
-    </div>
-    );
+        <h2>{galleryName}</h2>
+        <Page>
+          <Row>
+            {this.renderImages(artworks)}
+          </Row>
+        </Page>
+      </div>);
   }
 }
 
@@ -45,7 +34,7 @@ BlogPost.propTypes = {
 
 export default BlogPost;
 
-export const pageQuery = graphql`
+export const pageQuery = graphql `
   query blogPostQuery($slug: String!){
     contentfulGallery(slug: {eq: $slug}) {
       galleryName
